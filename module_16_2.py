77306cb5dc3f59ec96f6ac4df9f13e53c5f45b12
Nyzlo4ka <nyzlo4ka@mail.ru>
2024-11-10 09:58:22 +0300

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get("/")
+async def read_main() -> dict:
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+async def read_admin() -> dict:
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+async def read_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID')])\
+        -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+@app.get("/user/{username}/{age}")
+async def read_user_info(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
+                         age: int = Path(ge=18, le=120, description='Enter age')):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
